4611bc3fa7f071b75a6f3d263d5a1cb27019fbf7
Vishnu P <24x7vishnu@gmail.com>
2024-12-13 13:34:42 +0530

    options added

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors(
   {
     origin: process.env.CLIENT_URL,
     credentials:true,
-    methods:['GET', 'POST','PUT','PATCH','DELETE'],
+    methods:['GET', 'POST','PUT','PATCH','DELETE','OPTIONS'],
   }
 ));
 
